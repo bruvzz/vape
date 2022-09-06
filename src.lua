57ea85ae3483ec304465d1a2392f225dbe762f71
Ducky <97487136+bruvzz@users.noreply.github.com>
2022-09-06 19:46:27 -0400

    Update src.lua

--- src.lua
+++ src.lua
@@ -238,7 +238,7 @@ function lib:Window(text, preset, closebind)
         local NotificationDesc = Instance.new("TextLabel")
 
         NotificationHold.Name = "NotificationHold"
-        NotificationHold.Parent = ducklib
+        NotificationHold.Parent = ui
         NotificationHold.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
         NotificationHold.BackgroundTransparency = 1.000
         NotificationHold.BorderSizePixel = 0
